e507fc1bf07c976ed8a04e6d601d5eef1f206923
Lysander Fontyn <lysander.fontyn@zerofisher.dev>
2026-01-30 19:31:48 +0100

    Add additional inputs and configure host-specific options for Aurora in `flake.nix`
    This introduces several dependencies that the hosts and their modules will rely on. The Nix flake serves as the entry point for each NixOS system. It also comes with a lock file, `flake.lock`, which pins the flake's inputs to the latest commits at the time they were last updated.

--- flake.nix
+++ flake.nix
@@ -1,23 +1,100 @@
 {
-  description = "NixOS Configuration";
+  description = "NixOS Configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Packages
 
+    # User Environments
     home-manager = {
       url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # Secret Provisioning
+    sops-nix = {
+      url = "github:mic92/sops-nix/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Scrollable-Tiling Wayland Compositor
+    niri-flake = {
+      url = "github:sodiboo/niri-flake/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Rofi Cliphist Integration
+    rofi-tools = {
+      url = "github:szaffarano/rofi-tools/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Neovim Configuration
+    nixvim = {
+      url = "github:nix-community/nixvim/main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-flatpak.url = "github:gmodena/nix-flatpak/latest"; # Flatpak Declaration
+
+    # Visual Studio Code Extensions
+    nix4vscode = {
+      url = "github:nix-community/nix4vscode/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # SOPS Secrets
+    nixos-secrets = {
+      flake = false;
+      url = "git+ssh://git@github.com/0fficersally/nixos-secrets.git?ref=main&shallow=1";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = inputs@{
+    self,
+    nixpkgs,
+    home-manager,
+    sops-nix,
+    niri-flake,
+    rofi-tools,
+    nixvim,
+    nix-flatpak,
+    nix4vscode,
+    nixos-secrets,
+    ...
+  }: {
     nixosConfigurations = {
       aurora = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs;};
         modules = [
           ./hosts/aurora/configuration.nix
-          inputs.home-manager.nixosModules.default
+          home-manager.nixosModules.home-manager
+          sops-nix.nixosModules.sops
+          niri-flake.nixosModules.niri
+
+          {
+            hardware.nvidia.prime = {
+              intelBusId = "PCI:0:2:0"; # Integrated
+              nvidiaBusId = "PCI:1:0:0"; # Discrete
+            };
+
+            home-manager = {
+              sharedModules = [ sops-nix.homeManagerModules.sops ];
+
+              users.lysan.imports = [
+                ./hosts/aurora/home-configuration.nix
+                nixvim.homeModules.nixvim
+                nix-flatpak.homeManagerModules.nix-flatpak
+
+                {
+                  nixpkgs.overlays = [ nix4vscode.overlays.default ];
+                }
+              ];
+
+              extraSpecialArgs = { inherit self rofi-tools nixos-secrets; };
+            };
+          }
         ];
+
+        specialArgs = { inherit nixos-secrets; };
       };
     };
   };
