144c438798e58388ff4dc0f5ab972f2d082e89c7
Lysander Fontyn <lysander.fontyn@zerofisher.dev>
2026-03-05 18:36:41 +0100

    Create NixOS module for Caddy Docker container
    This adds a NixOS module for deploying the Caddy reverse proxy.

---
+++ modules/nixos/containers/caddy.nix
@@ -0,0 +1,26 @@
+# Reverse Proxy
+{ config, lib, caddy-compose, ... }: {
+  options = {
+    modules.containers.caddy.enable = lib.mkEnableOption "the Caddy reverse proxy";
+  };
+
+  config = lib.mkIf config.modules.containers.caddy.enable {
+    systemd.services.caddy = {
+      description = "Caddy Reverse Proxy";
+      requires = [ "docker.service" ];
+      after = [ "docker.service" ];
+      environment.CADDY_EMAIL = "lysander.fontyn@zerofisher.dev";
+
+      serviceConfig = let docker = lib.getExe pkgs.docker; in {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        WorkingDirectory = caddy-compose;
+        ExecStart = "${docker} compose up --remove-orphans --detach";
+        ExecStop = "${docker} compose stop";
+        TimeoutStopSec = "30s";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
