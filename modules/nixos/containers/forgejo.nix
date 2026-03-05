61256aa8807cb25699d2d5624b693b062c69b15d
Lysander Fontyn <lysander.fontyn@zerofisher.dev>
2026-03-05 18:40:27 +0100

    Create NixOS module for Forgejo Docker stack
    This adds a NixOS module for deploying the Forgejo software forge.

---
+++ modules/nixos/containers/forgejo.nix
@@ -0,0 +1,29 @@
+# Software Forge
+{ config, lib, forgejo-compose, ... }: {
+  options = {
+    modules.containers.forgejo.enable = lib.mkEnableOption "the Forgejo software forge";
+  };
+
+  config = lib.mkIf config.modules.containers.forgejo.enable {
+    modules.containers.caddy.enable = true; # Reverse Proxy
+    sops.secrets."environment-variables/forgejo" = {};
+
+    systemd.services.forgejo = {
+      description = "Forgejo Software Forge";
+      requires = [ "docker.service" "caddy.service" ];
+      after = [ "docker.service" "caddy.service" ];
+
+      serviceConfig = let docker = lib.getExe pkgs.docker; in {
+        Type = "oneshot";
+        RemainAfterExit = "yes";
+        WorkingDirectory = forgejo-compose;
+        EnvironmentFile = config.sops.secrets."environment-variables/forgejo".path;
+        ExecStart = "${docker} compose up --remove-orphans --detach";
+        ExecStop = "${docker} compose stop";
+        TimeoutStopSec = "30s";
+      };
+
+      wantedBy = [ "multi-user.target" ];
+    };
+  };
+}
