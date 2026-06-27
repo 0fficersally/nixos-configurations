f5e2530b25872fe06fb4dc945fe691a33a3d4fc1
Lysander Fontyn <lysander.fontyn@zerofisher.dev>
2026-06-27 16:54:02 +0200

    Update Ludusavi root paths for `heroic` and `steam`

--- modules/home-manager/applications/gui/ludusavi.nix
+++ modules/home-manager/applications/gui/ludusavi.nix
@@ -19,7 +19,7 @@
 
           {
             store = "heroic";
-            path = "${config.home.homeDirectory}/Games/Heroic";
+            path = "/mnt/Games/Heroic";
           }
 
           {
@@ -29,7 +29,7 @@
 
           {
             store = "steam";
-            path = "${config.home.homeDirectory}/Games/Steam";
+            path = "/mnt/Games/Steam";
           }
         ];
 
