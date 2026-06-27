01beb0f77956afe4c31111cece950c854cdeee5b
Lysander Fontyn <lysander.fontyn@zerofisher.dev>
2026-06-27 17:50:12 +0200

    Update Zsh configuration path

--- modules/home-manager/environments/terminal/shells/zsh.nix
+++ modules/home-manager/environments/terminal/shells/zsh.nix
@@ -7,6 +7,7 @@
   config = lib.mkIf config.modules.environments.terminal.shells.zsh.enable {
     programs.zsh = {
       enable = true;
+      dotDir = "${config.xdg.configHome}/zsh"; # Default in State Version `26.05`
 
       initContent = lib.mkAfter ''
         eval "$(starship init zsh)"
